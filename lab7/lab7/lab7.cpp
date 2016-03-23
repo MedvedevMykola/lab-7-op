86ae8e6fb4c7c7a4f0a29fe1475046e53b38723f
test <medved85755@gmail.com>
2016-03-23 22:33:54 +0200

    95%

--- lab7/lab7/lab7.cpp
+++ lab7/lab7/lab7.cpp
@@ -10,13 +10,24 @@ void input(char *s, char *e)
 	gets_s(s, n);
 	puts("Enter group of symbols:");
 	gets_s(e, n);
-
 }
-
+int num(char*s, char*e)
+{
+	int k = 0;
+	char *delimiter = " .,?!:;\/(){}[]";
+	char *str;
+	str = strtok(s, delimiter);
+	while (str != NULL)
+	{
+		if ( strrstr(str, e) == str)k++;
+	}
+	return k;
+}
 int main()
 {
 	char s[n], e[n];
 	input(s, e);
+	printf("word deleted:%d",num(s, e));
 	_getch();
 }
 
